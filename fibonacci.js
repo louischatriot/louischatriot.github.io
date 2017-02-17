f9d9c92501dec09081d156013d5f39d4555331bf
Louis Chatriot <lchatriot@stripe.com>
2017-02-17 08:24:32 +0100

    Some logging to help demo

--- fibonacci.js
+++ fibonacci.js
@@ -47,3 +47,26 @@ function fibLog (n) {
 
   return Math.pow(fibLog(Math.floor(n / 2) + 1), 2) - Math.pow(-1, n) * Math.pow(fibLog(Math.floor((n - 1) / 2)), 2)
 }
+
+
+
+console.log("*************************************");
+console.log("Recursive version (fibRec) - Exponential");
+console.log("Try n = 5, n = 10, n = 41, n = 42");
+console.log("");
+console.log("*************************************");
+console.log("Iterative version (fibLinear) - Linear");
+console.log("n = 41, n = 42, n = 100");
+console.log("");
+console.log("*************************************");
+console.log("Binet formula version (fibBinet) - Logarithmic, but ...");
+console.log("n = 15, n = 32, n = 40, n = 60");
+console.log("");
+console.log("*************************************");
+console.log("Matrix formula version (fibLog) - Logarithmic, but ...");
+console.log("n = 15, n = 32, n = 40, n = 60");
+console.log("");
+
+
+
+
