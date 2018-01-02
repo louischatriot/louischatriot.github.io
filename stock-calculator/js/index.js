46bf5979fc2c7ae5e3ad3067ddaa370d50a97a28
Louis Chatriot <louis.chatriot@gmail.com>
2018-01-02 15:56:01 +0100

    Able to creaate graphs

--- stock-calculator/js/index.js
+++ stock-calculator/js/index.js
@@ -62,8 +62,10 @@ function calculate () {
 
   if (isNaN(gains.buyerGain)) {
     $('#output').css('display', 'none');
+    $('#graph').css('display', 'none');
   } else {
     $('#output').css('display', 'block');
+    $('#graph').css('display', 'block');
 
     $('#buyer-paid').html(formatAmount(optionExercisePrice));
     $('#buyer-gain').html(formatAmount(gains.buyerGain));
@@ -72,113 +74,90 @@ function calculate () {
     $('#buyer-annual-growth').html(formatPercent(buyerGainPercentAnnual));
 
     $('#seller-gain').html(formatAmount(gains.sellerGain));
+
+    // GRAPH
+    var buyer = [], seller = [];
+    for (var i = 0; i < 10; i += 0.1) {
+      gains = calculateGain(optionExercisePrice, i, yearsBeforeLiquidity, cutoffCAGR, shareOfGains, numberOptions);
+      buyer.push({ stockPrice: i, gain: gains.buyerGain });
+      seller.push({ stockPrice: i, gain: gains.sellerGain });
+    }
+
+
+    $('#graph').attr('width', $('#graph').width());
+    $('#graph').attr('height', $('#graph').height());
+    $('#graph').html('');
+    var svg = d3.select("svg#graph"),
+        margin = {top: 20, right: 80, bottom: 30, left: 50},
+        width = svg.attr("width") - margin.left - margin.right,
+        height = svg.attr("height") - margin.top - margin.bottom,
+        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var parseTime = d3.timeParse("%Y%m%d");
+
+    var x = d3.scaleLinear().range([0, width]),
+        y = d3.scaleLinear().range([height, 0]),
+        z = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var line = d3.line()
+        //.curve(d3.curveBasis)
+        .x(function(d) { return x(d.stockPrice); })
+        .y(function(d) { return y(d.gain); });
+
+
+
+      var data = buyer;   // We don't care which as long as we got the full width of the x axis
+      var cities = [ { id: 'buyer', values: buyer }, { id: 'seller', values: seller } ];
+
+      x.domain(d3.extent(data, function(d) { return d.stockPrice; }));
+
+      y.domain([
+        d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.gain; }); }),
+        d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.gain; }); })
+      ]);
+
+      z.domain(cities.map(function(c) { return c.id; }));
+
+      g.append("g")
+          .attr("class", "axis axis--x")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x))
+        .append("text")
+          .attr("x", width)
+          .attr("dy", "-0.71em")
+          .attr("fill", "#000")
+          .text("Stock price ($)");
+
+      g.append("g")
+          .attr("class", "axis axis--y")
+          .call(d3.axisLeft(y))
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", "0.71em")
+          .attr("fill", "#000")
+          .text("Unit gain ($)");
+
+      var city = g.selectAll(".city")
+        .data(cities)
+        .enter().append("g")
+          .attr("class", "city");
+
+      city.append("path")
+          .attr("class", "line")
+          .attr("d", function(d) { return line(d.values); })
+          .style("stroke", function(d) { return z(d.id); });
+
+      city.append("text")
+          .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
+          .attr("transform", function(d) { return "translate(" + x(d.value.stockPrice) + "," + y(d.value.gain) + ")"; })
+          .attr("x", 3)
+          .attr("dy", "0.35em")
+          .style("font", "10px sans-serif")
+          .text(function(d) { return d.id; });
+
+      // /GRAPH
   }
-
-
-  // GRAPH
-$('#graph').attr('width', $('#graph').width());
-$('#graph').attr('height', $('#graph').height());
-var svg = d3.select("svg#graph"),
-    margin = {top: 20, right: 80, bottom: 30, left: 50},
-    width = svg.attr("width") - margin.left - margin.right,
-    height = svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  console.log(width);
-  console.log(height);
-
-var parseTime = d3.timeParse("%Y%m%d");
-
-var x = d3.scaleLinear().range([0, width]),
-    y = d3.scaleLinear().range([height, 0]),
-    z = d3.scaleOrdinal(d3.schemeCategory10);
-
-var line = d3.line()
-    //.curve(d3.curveBasis)
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.gain); });
-
-
-var buyer = [
-    { date: 1, gain: 1 }
-  , { date: 2, gain: 2 }
-  , { date: 3, gain: 5 }
-  , { date: 4, gain: 10 }
-  , { date: 5, gain: 16 }
-  ];
-
-var seller = [
-    { date: 1, gain: 0 }
-  , { date: 2, gain: 0 }
-  , { date: 3, gain: 0 }
-  , { date: 4, gain: 20 }
-  , { date: 5, gain: 25 }
-  ];
-
-  var data = buyer;   // We don't care which as long as we got the full width of the x axis
-  var cities = [ { id: 'buyer', values: buyer }, { id: 'seller', values: seller } ];
-
-  //var cities = data.columns.slice(1).map(function(id) {
-    //return {
-      //id: id,
-      //values: data.map(function(d) {
-        //return {date: d.date, gain: d[id]};
-      //})
-    //};
-  //});
-
-  //console.log(d3.extent(data, function(d) { return d.date; }));
-  //console.log( d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.gain; }); }) );
-
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-
-  y.domain([
-    d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.gain; }); }),
-    d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.gain; }); })
-  ]);
-
-  z.domain(cities.map(function(c) { return c.id; }));
-  //console.log(cities.map(function(c) { return c.id; }));
-
-  g.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-    .append("text")
-      .attr("x", width)
-      .attr("dy", "-0.71em")
-      .attr("fill", "#000")
-      .text("Stock price ($)");
-
-  g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y))
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("fill", "#000")
-      .text("Unit gain ($)");
-
-  var city = g.selectAll(".city")
-    .data(cities)
-    .enter().append("g")
-      .attr("class", "city");
-
-  city.append("path")
-      .attr("class", "line")
-      .attr("d", function(d) { return line(d.values); })
-      .style("stroke", function(d) { return z(d.id); });
-
-  city.append("text")
-      .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.gain) + ")"; })
-      .attr("x", 3)
-      .attr("dy", "0.35em")
-      .style("font", "10px sans-serif")
-      .text(function(d) { return d.id; });
-
-  // /GRAPH
-
 
 
 }
